9bb23618e3c39cfa23bd7b7b50a7d4c888f30a91
WILLIAN NUNES PARENTE <willianparente@hotmail.com>
2020-12-17 14:32:25 -0300

    add remote sql

--- src/main/java/br/com/gamaitauprojetofinalbackspringboot/dao/TransactionDAO.java
+++ src/main/java/br/com/gamaitauprojetofinalbackspringboot/dao/TransactionDAO.java
@@ -1,9 +1,22 @@
 package br.com.gamaitauprojetofinalbackspringboot.dao;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import br.com.gamaitauprojetofinalbackspringboot.beans.Transaction;
 
 public interface TransactionDAO extends CrudRepository<Transaction, Integer>{
 
+	@Query(value= "SELECT COUNT(id_transaction) AS TOTAL FROM mtb310_transaction WHERE ag_financeiro_id_agente = :num", nativeQuery=true)
+	public int findByTotalTransactons(@Param("num") int num);
+	
+	@Query(value= "SELECT COUNT(id_transaction) AS TOTAL FROM mtb310_transaction WHERE ag_financeiro_id_agente = :num AND ", nativeQuery=true)
+	public int findByTotalSucesso(@Param("num") int num);
+	
+	@Query(value= "SELECT COUNT(id_transaction) AS TOTAL FROM mtb310_transaction WHERE ag_financeiro_id_agente = :num", nativeQuery=true)
+	public int findByTotalFalhas(@Param("num") int num);
+	
+	@Query(value= "SELECT COUNT(id_transaction) AS TOTAL FROM mtb310_transaction WHERE ag_financeiro_id_agente = :num", nativeQuery=true)
+	public int findByTotalFraudes(@Param("num") int num);
 }
