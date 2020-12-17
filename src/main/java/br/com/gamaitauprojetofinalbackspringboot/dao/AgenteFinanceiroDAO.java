32648c2c5780600b9bca3f09c7aeece1048728f5
WILLIAN NUNES PARENTE <willianparente@hotmail.com>
2020-12-17 17:23:19 -0300

    bug fix

--- src/main/java/br/com/gamaitauprojetofinalbackspringboot/dao/AgenteFinanceiroDAO.java
+++ src/main/java/br/com/gamaitauprojetofinalbackspringboot/dao/AgenteFinanceiroDAO.java
@@ -9,7 +9,8 @@ import br.com.gamaitauprojetofinalbackspringboot.beans.AgenteFinanceiro;
 
 public interface AgenteFinanceiroDAO extends CrudRepository<AgenteFinanceiro, Integer>  {
 	
-	@Query(value= "SELECT ag.id_agente, ag.nome_agente, ag.volume_transacional FROM mtb310_ag_financeiro "
+	@Query(value= "SELECT id_agente, nome_agente, volume_transacional "
+			+ "FROM mtb310_ag_financeiro "
 			+ "ORDER BY volume_transacional DESC "
 			+ "LIMIT 10;", nativeQuery=true)
 	public List<AgenteFinanceiro> findTopTen();
